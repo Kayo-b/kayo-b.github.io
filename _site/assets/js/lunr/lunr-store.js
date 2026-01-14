9059dd6b73c26312d006e1dd932b49f087e8d3ab
kayo-b <kyo.bhz@gmail.com>
2026-01-14 07:20:57 +0100

    trying to fix react bundle rendering inside jekyll

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Web Security Fundamentals - HTB Course Notes",
-        "excerpt":"Working with HTTP Requests Understanding cURL The curl command is your bread and butter for manual HTTP testing. It’s what you reach for when you need to interact with web servers without the overhead of a browser. Basic usage with verbosity: curl &lt;url&gt; -v # basic verbose output curl &lt;url&gt;...","categories": ["infosec","web-security"],
+        "title": "Web Security - HTB Course",
+        "excerpt":"cURL Commands curl &lt;url&gt; -v # verbose curl &lt;url&gt; -vvv # max verbosity curl -I &lt;url&gt; # HEAD request (headers only) curl -i &lt;url&gt; # headers + body curl &lt;url&gt; -A 'Mozilla/5.0' # change User-Agent SSH config: /etc/ssh/sshd_config Authentication # HTTP Basic Auth curl -u admin:admin http://&lt;SERVER_IP&gt;:&lt;PORT&gt;/ curl http://admin:admin@&lt;SERVER_IP&gt;:&lt;PORT&gt;/ #...","categories": ["infosec","web-security"],
         "tags": ["web-security","curl","http","xss","csrf","sql-injection","htb"],
         "url": "/infosec/web-security/web-security-htb-course-notes/",
+        "teaser": null
+      },{
+        "title": "Web Security - Fuzzing",
+        "excerpt":"Overview Fuzzing uncovers hidden vulnerabilities through automated security testing, strengthening input validation, improving code quality, and enabling continuous security. Fuzzing integrates into the software development lifecycle as part of CI/CD pipelines. Essential Concepts: Wordlist Payload Response analysis fuzzer False positive/negative Fuzzing scope Fuzzing Tools Requirements Go Python3 pipx (set ensurepath...","categories": ["infosec","web-security"],
+        "tags": ["fuzzing","ffuf","gobuster","feroxbuster","wenum","subdomain-enumeration","directory-enumeration","htb"],
+        "url": "/infosec/web-security/web-security-fuzzing/",
         "teaser": null
       }]
